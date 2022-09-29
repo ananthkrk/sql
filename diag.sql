6e5bdd193d8e56b322286540b51348caf062cfe2
ananthkrk <30938917+ananthkrk@users.noreply.github.com>
2022-09-28 22:27:27 -0700

    Update diag.sql

--- diag.sql
+++ diag.sql
@@ -31,81 +31,76 @@ declare
 	);
 	type tbl is table of rec;
 	tb   tbl;
+	
+	begin
+		if pSum == 'Y' then
+			l_hdr 	:= RPAD('SUMMARY .....', 80);
+			l_inp 	:= chr(39)|| REPLACE(pWhr, ',', CHR(39)||','||CHR(39))||CHR(39);
+			l_query := 'select stat_type, ';
+			l_dtl 	:= 'select rpad(stat_type, 80)||';
+		else
+			l_hdr 	:= RPAD('DETAILS .....', 80);
+			l_query := 'select stat_type, ';
+			l_dtl 	:= 'select rpad(stat_type, 80)||';
+		end if;
+		
+		open cr;
+		fetch cr bulk collect into tb;
+		for i in 1..tb.count loop
+			if tb.exists (i-1) then
+				l_dff := l_dff || 'delta' || tb(i).runnm||tb(i).runid||' val > 0'; 
+				l_sel := l_sel || ', sum(nvl(' || tb(i).runnm || tb(i).runid || '_val, 0 )) - sum(nvl(' || tb(i-1).runnm || tb(i-1).runid || '_val, 0 )) as '|| 'delta' || tb(i).runnm ||  tb(i).runid || '_val';
+				l_dtl := l_dtl || 'lpad(delta)' ||  tb(i).runnm || tb(i).runid || '_val'||',25)||';
+				l_hdr := l_hdr || substr(lpad('delta'|| tb(i).runnm || tb(i).runid, 26), 1, length(lpad('delta'|| tb(i).runnm || tb(i).runid, 26))-1); 
+			else
+				l_sel := l_sel || ', sum(nvl(' || tb(i).runnm || tb(i).runid || '_val, 0 )) as '|| 'delta' || tb(i).runnm ||  tb(i).runid || '_val';
+				l_dtl := l_dtl || 'lpad(delta)' ||  tb(i).runnm || tb(i).runid || '_val'||',25)||';
+				l_hdr := l_hdr || substr(lpad('delta'|| tb(i).runnm || tb(i).runid, 26), 1, length(lpad('delta'|| tb(i).runnm || tb(i).runid, 26))-1); 								
+			end if;
+			l_pvt := l_pvt || tb(i).runid || ' as ' || tb(i).runnm || tb(i).runid || ',';
+		end loop;
+		close cr;
+		
+		1_pvt :=  substr(l_pvt, 1, length(l_pvt) - 1);
+		l_dtl :=  substr(l_dtl, 1, length(l_dtl) - 1);
+		l_dff :=  substr(l_dff, 1, length(l_dff) - 1);
+		l_whr :=  l_whr || ' and ('|| l_dff ||') ';
+		
+		if pSUM = 'Y' then
+			l_query :=  l_query  || l_sel || ' from run_stats puvot ( sum(stat_val) as val ' || l_pvt || ') ) where  stat_type in ('|| l_inp || ')' || ' group by stat_type ';
+		else
+			l_query :=  l_query  || l_sel || ' from run_stats puvot ( sum(stat_val) as val ' || l_pvt || ') ) where  stat_type = ' || chr(39) || pwhr || chr(39) || ' group by stat_type ';
+		end if;
+		po_hdr  := l_hdr;
+		open po_cursor for l_dtl || ' txt from ( '|| l_query ||' ) ' || l_whr;
+	end;
+	procedure rs start 
+	is 
+	begin
+	
+		select distinct sid into p_sid from v$mystat; 
+		execute immediate 'truncate table run stats':
+		insert into run_stats (runid, runnm, stat_type, stat_name, stat_val)
+		(
+			select 0, 'BEFORE', 'STATISTIC' a.name name, b.value from vSstatname a, vmystat b where a.statistic# b.statistic#
+			union all
+			select 0, 'BEFORE', 'LATCH' ,name, gets from v$latch
+			union all
+			select 0, 'BEFORE', 'TIMER' ,'hsecs' name, hsecs from v$v$timer
+			union all
+			select 0, 'BEFORE', 'TIME' ,stat_name name, value from v$sess_time_model where sid = p_sid
+			union all
+			select 0, 'BEFORE', 'WAIT' ,event name , time_waited_micro from v$session_event where sid = p_sid
+		);
+		commit;
+		g_start := dbms_utility.get_cpu_time;
+		end;
 
-
-
-
-
-
-
-for i in 1..tb.count loop
-if tb.exists (i-1) then
-1 dff := 1 dff Il 'delta' litb(i).runnm||tb(i).runid|l' val > 0 or sel
-It' val
-1 dtl
-sum (nvl ('|1 tb(i) .runnm||tb(j) .runidll' _val,0)) - sum(nV1 (' |1 tb(i-1) .runnm/Itb(i-1) .runid||' _val, 0)) as 'I| 'delta' Nitb(i).runnm|Itb(i).runid
-1 hdr
-1 dtl 11 'Ipad (delta' | Itb (i) .runnm| (tb(i) .runid||' _val'll ,25)111;
-else
-1 har Il substr (Lpad ('delta'| Itb (i) .runnm| Itb(j) . runid, 26) ,1, length (1pad ('delta' IItb (i) . runnm| Itb (i) runid, 26))-1);
-1 sel := 1 sel
-1 dtl
-1 hdr end if;
-1 pvt end loop;
-'sum (nvl (' |1 tb(i) .runnm| Itb(i) .runidl|' val,0)) as "Iltb(i) .runnm| Itb(i) .runidll' _val';
-'Apad (' litb (i) .runna| Itb(i) . runidll' val"ll? ,25) 1P':
-:- 1 hdr il substr (1pad (tb (i) . zunnm| Itb (i) . runid,26) ,1, length (1pad (tb(i) . runnmI Itb(i) -runid, 26))-1) ;
-1pvt 11 tb(i) ranidll" as "Iltb(i) .runnnlItb(i).runidli
-close cr;
-1_pvt
-1 del
-substr (1 _pvt, 1,length (1_pvt) -1) substr(1_dtl,1,length(1 dtl)-2):
-1 dff substr (2_dtf, 1, length (2 atFs-):
-wnr
-L whr it
-• and ('11 l def Il')'
-1F pSUM -
-ry' then
-query else
-- 1 query I| 1_sel Il ' from run stats pivot ( sum(stat_val) as vat 'I| 1 pvt I/') ) where stat_type in
-('11 1 inpll ") 'Il' group by stat_type
-1_query :- 1 query I| 1_gel Il ' from run stats pivot ( sum(stat_val) as val 'I| 1 pvt Il')) where stat_type = '11 chr (39) |1 pwhrIl chr (39) ||' group by stat_type,
-stat name end 1f;
-po_header:- 1 _hdr;
---dbms_output.put line (1 dtlIl ' txt from ('|| 1 query (1'y open po_cursor for
-1 whr);
-end;
-1 dtl 11 " txt from ('ll 1 query Il*) ' 11 1_whr;
-procedure rs start is bedin
-select distinct sid into p sid from v$mystat; execute immediate 'truncate table run stats':
-insert into run_stats (runid, runnm, stat_type, stat_name, stat_val)
-select 0, 'BEFORE', "STATISTIC' a.name name, b.value from vSstatname a, vmystat b where
-union all
-a.statistich b.statistic:
-selecu, union a11
-'BEFORE', "LATCH' ,name, gets from v$latch
-select U,
-1 BEFORFI
-, 'TIMER', 'hsecs!
-name, hsecs from v$timer
-union al select u.
-"BEFORE;
-TINE
-union all
-stat_name name, value from vSsess_time_model where sid a p sid
-select 0,
-'BEFORE', "WAIT'
-event name
-time waited micro from VSSESSION_EVENT where sid - P_sid
-start
-= dbms_utility.get cpu
 
 
 Pg3
 
 
-9_start:= dbms_utility.get cpu time;
-end;
 procedure rs middle (pstep in varchar2)
 is
 1 runid number:
@@ -141,19 +136,13 @@ fetch 1 cursor into 1 x; exit when 1 cursortnotfound; dbms_output.put_line (1_x)
 end loop;
 close 1 cursor;
 dbms_output.put_line (chr (9)) ;
-
 buildgry ('STATISTIC', 'N' ,1 _CUrSOr, 1 _header) dibms output.put line (1 header);
 Toor
 fetch 1 cursor into 1 x; exit when 1 cursortnotfound;
 dibms output.put line (1 x);
 end loop:
 dbms_ output.put_line (chr (9));
-
-
-
-
 Pg4
-
 ]
 Loop fetch 1 cursor into 1 x;
 exit when 1 cursor&not found;
